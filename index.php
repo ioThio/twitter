1d9491d17d9330c8c11e49542a5da6c9edaf2fc3
Ioanna Theofilakou <io.theofilakou@gmail.com>
2019-12-26 03:28:06 +0200

    Update index.php

--- index.php
+++ index.php
@@ -23,10 +23,10 @@ Search <img src="twitter.png" alt="twitter image"><span>Twitter</span> for: <br>
   require_once('TwitterAPIExchange.php');
   /** Set access tokens here - see: https://dev.twitter.com/apps/ **/
   $settings = array(
-  'oauth_access_token' => "1206684657686917122-Pz4RCwIW051u1uqsjSKlD5T3w8c2YW",
-  'oauth_access_token_secret' => "M1QN8j0uFl0QZLpniKDjz8dGgBVFlcgAnqJGEFMnJ8WsX",
-  'consumer_key' => "pqc8834zdCU4Pq8VIm5OmepwJ",
-  'consumer_secret' => "hTTZmIkL4vCVi7dHqeU2p2LT58ohwtawwJDGTjhVlkTBaNOCvr"
+  'oauth_access_token' => "ACCESS_TOKEN",
+  'oauth_access_token_secret' => "ACCESS_TOKEN_SECRET",
+  'consumer_key' => "CONSUMER_KEY",
+  'consumer_secret' => "CONSUMER_SECRET"
   );
   $url = "https://api.twitter.com/1.1/search/tweets.json?q=$_POST['keyword']&src=typed_query";
   $requestMethod = "GET";
@@ -40,13 +40,13 @@ Search <img src="twitter.png" alt="twitter image"><span>Twitter</span> for: <br>
   if(array_key_exists("errors", $string)) {echo "<h3>Sorry, there was a problem.</h3><p>Twitter returned the following error message:</p><p><em>".$string[errors][0]["message"]."</em></p>";exit();}
   foreach($string as $items)
       {
-          // echo "Time and Date of Tweet: ".$items['created_at']."<br />";
+          echo "Time and Date of Tweet: ".$items['created_at']."<br />";
           echo "Tweet: ". $items['full_text']."<br />";
           echo "Tweeted by: ". $items['user']['name']."<br />";
-          // echo "Screen name: ". $items['user']['screen_name']."<br />";
-          // echo "Followers: ". $items['user']['followers_count']."<br />";
-          // echo "Friends: ". $items['user']['friends_count']."<br />";
-          // echo "Listed: ". $items['user']['listed_count']."<br /><hr />";
+          echo "Screen name: ". $items['user']['screen_name']."<br />";
+          echo "Followers: ". $items['user']['followers_count']."<br />";
+          echo "Friends: ". $items['user']['friends_count']."<br />";
+          echo "Listed: ". $items['user']['listed_count']."<br /><hr />";
       }
   ?>
 
